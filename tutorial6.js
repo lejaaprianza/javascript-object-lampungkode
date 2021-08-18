eb6417ca7711c12bac7fa2d7842b51fde7352c57
lejaaprianza <lejaprianza@gmail.com>
2021-08-18 09:03:14 +0700

    tutorial 6 dan update tutorial 5

--- tutorial6.js
+++ tutorial6.js
@@ -1,22 +1,43 @@
-let x = ""
-const wilayah = {
-  negara: "Indonesia",
-  umur: 76,
-  daerah: [
-    {
-      provinsi: "Lampung",
-      kota: ["Bandar Lampung", "Metro", "Lampung Selatan"],
-    },
-    { provinsi: "Maluku", kota: ["Ambon", "Tual", "Maluku Tengah"] },
-    { provinsi: "Jawa Barat", kota: ["Bandung", "Majalengka", "Bogor"] },
-  ],
-}
+const mobil = [
+  { mobil1: "honda", mobil2: "fortuner", mobil3: "daihatsu" },
+  { mobil4: "tesla", mobil5: "toyota", mobil6: "jeep" },
+]
+console.log(mobil)
+console.log(mobil[0])
+console.log(mobil[0].mobil1)
 
-for (let i in wilayah.daerah) {
-  x += "\n\nIni Provinsi " + wilayah.daerah[i].provinsi + "\n"
-  for (let j in wilayah.daerah[i].kota) {
-    x += wilayah.daerah[i].kota[j] + "\n"
-  }
-}
+const motor = { brand: ["supra", "mio", "vario"] }
+console.log(motor)
+console.log(motor.brand)
+console.log(motor.brand[0])
 
-console.log(x)
+// const wilayah = {
+//   negara: "Indonesia",
+//   umur: 76,
+//   daerah: [
+//     {
+//       provinsi: "Lampung",
+//       kota: ["Bandar Lampung", "Metro", "Lampung Selatan"],
+//     },
+//     { provinsi: "Maluku", kota: ["Ambon", "Tual", "Maluku Tengah"] },
+//     { provinsi: "Jawa Barat", kota: ["Bandung", "Majalengka", "Bogor"] },
+//   ],
+// }
+
+// console.log(wilayah.daerah)
+
+// for (let i in wilayah.daerah) {
+//   console.log(wilayah.daerah[i].provinsi)
+//   console.log(i)
+//   console.log("tes")
+// }
+
+// let daftarDaerah = ""
+// for (let i in wilayah.daerah) {
+//   daftarDaerah += "\n\nIni Provinsi " + wilayah.daerah[i].provinsi + "\n"
+//   for (let j in wilayah.daerah[i].kota) {
+//     daftarDaerah += wilayah.daerah[i].kota[j] + "\n"
+//   }
+// }
+
+// console.log(daftarDaerah)
