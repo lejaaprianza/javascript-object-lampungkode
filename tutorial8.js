57178b322e7d98de42c9d1067f57ace8252c5dc5
lejaaprianza <lejaprianza@gmail.com>
2021-08-19 09:06:21 +0700

    tutorial 8

--- tutorial8.js
+++ tutorial8.js
@@ -1,42 +1,18 @@
-const rows = [
-  {
-    username: "Andi",
-    age: 20,
+const daerah = {
+  namaProvinsi: "Lampung",
+  namaKota: "Bandar Lampung",
+  alamat: "rahasia",
+  nomor: {
+    angka1: 1,
+    angka2: 2,
+    angka3: 3,
   },
-  {
-    username: "Agus",
-    age: 20,
-  },
-  {
-    username: "Bagus",
-    age: 20,
-  },
-  {
-    username: "Bobi",
-    age: 20,
-  },
-  {
-    username: "Rian",
-    age: 20,
-  },
-]
-
-const testingData = {
-  data1: "tes1",
-  data2: { nestData: {} },
-  data3: "tes3",
-  data4: "tes4",
-  data5: "tes5",
 }
 
-const hasil = []
-for (i in testingData) {
-  hasil.push({ [i]: testingData[i] })
-}
-console.log(hasil)
+delete daerah.alamat
 
-for (i in rows) {
-  Object.assign(rows[i], hasil[i])
-}
+console.log(daerah)
 
-console.log(rows)
+delete daerah.nomor.angka1
+
+console.log(daerah)
