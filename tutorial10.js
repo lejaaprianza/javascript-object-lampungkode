bc021fe6c027c7342eb5ca5d652c5e4adcd54b4c
lejaaprianza <lejaprianza@gmail.com>
2021-08-21 12:57:38 +0700

    tutorial 10

--- tutorial10.js
+++ tutorial10.js
@@ -1,8 +1,20 @@
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  id: 5566,
-  fullName: function () {
-    return this.firstName + " " + this.lastName
-  },
+//contoh 1
+const biodataSaya = {
+  nama: "Leja Aprianza",
+  usia: 17,
+  asal: "Lampung",
 }
+
+console.log("Ini contoh satu")
+const biodataArray = Object.values(biodataSaya)
+console.log(biodataArray)
+
+//contoh 2
+const biodataSaya2 = {
+  orang1: { nama: "Leja Aprianza", usia: 17, asal: "Lampung" },
+  orang2: { nama: "Zulaika", usia: 17, asal: "Jawa Barat" },
+}
+
+console.log("\nIni contoh dua")
+const biodataArray2 = Object.values(biodataSaya2)
+console.log(biodataArray2)
