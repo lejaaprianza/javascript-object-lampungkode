3b0aacb871996eb9c8aa24cd607ddbb955a59f75
lejaaprianza <lejaprianza@gmail.com>
2021-08-23 04:41:21 +0700

    tutorial 12

---
+++ tutorial12.js
@@ -0,0 +1,11 @@
+const contoh = {
+  nama: "Leja Aprianza",
+  daerah: "Lampung",
+  kalimat: "",
+  set sentence(x) {
+    this.kalimat = x + this.nama + ", " + this.daerah
+  },
+}
+
+// contoh.sentence = "Biodata: "
+console.log(contoh.kalimat)
